fe7686c8561ecb487bba32d4f6417190e4ce46dd
Dmitry Ivanov <dimasuperior@gmail.com>
2020-06-10 02:26:17 +0300

    Выполняет задание "шаблонизируй это"

--- src/express/index.js
+++ src/express/index.js
@@ -1,17 +1,24 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 const offersRoutes = require(`./routes/offers`);
 const myRoutes = require(`./routes/my`);
 const mainRoutes = require(`./routes/main`);
 
+const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
-const port = 8080;
 
 
+app.set(`views`, `${__dirname}/templates`);
+app.set(`view engine`, `pug`);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/offers`, offersRoutes);
 
-app.listen(port);
+
+app.listen(DEFAULT_PORT);
